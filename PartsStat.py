c2a366a0a8bd861ecbfc3578b49d570e09850f1a
Julia Salnikova <agereth@gmail.com>
2018-07-27 19:42:56 +0300

    refactored

--- PartsStat.py
+++ PartsStat.py
@@ -9,39 +9,33 @@ components = {}
 
 def get_safe_data(BOMfilename:str)->(int, list):
     """
+    opens file BOMfilename as csv and returns data (exclude headers) and value column index as any
+    if dercode error or no value column returns None
+    :param BOMfilename: filename
+    :return: value column index, data
+    """
+    with open(BOMfilename, encoding='utf-8') as f:
+        data = csv.reader(f)
+        try:
+            data = list(data)
+        except UnicodeDecodeError:
+            print("Decode error in %s" % BOMfilename)
+            return None, None
+        csv_headers = data[0]
+        headers_corrected = [header.strip().lower() for header in csv_headers]
+        try:
+            return headers_corrected.index('value'), data[1:]
+        except IndexError:
+            print('No value column in %s BOM' % BOMfilename)
+            return None, None
 
-    :param BOMfilename:
+
+def write_data_to_xls(result: list):
+    """
+    writes data to xls file
+    :param result: data
     :return:
     """
-
-def main():
-    for BOMfilename in os.listdir("."):
-        if BOMfilename.split('.')[1] == 'csv':
-            with open(BOMfilename, encoding='utf-8') as f:
-                data = csv.reader(f)
-                try:
-                    data = list(data)
-                except UnicodeDecodeError:
-                    print("Decode error in %s" % BOMfilename)
-                    continue
-                csv_headers = data[0]
-                headers_corrected = [header.strip().lower() for header in csv_headers]
-                try:
-                    i_value = headers_corrected.index('value')
-                except IndexError:
-                    print('No value column in %s BOM' % BOMfilename)
-                    continue
-                for row in data[1:]:
-                    value = row[i_value]
-                    if value in components.keys():
-                        components[value].append(BOMfilename.split('BOM')[0])
-                    else:
-                        components[value] = [BOMfilename.split('BOM')[0]]
-
-    data = [item for item in components.items() if not item[0] in dummies]
-    result = [(value, len(list(set(projects))), list(set(projects))) for (value, projects) in data]
-    result.sort(key=lambda x:x[1], reverse=True)
-
     book = xlwt.Workbook(encoding="utf-8")
     sheet1 = book.add_sheet("Statistics")
     sheet1.write(0, 0, "Value")
@@ -52,8 +46,26 @@ def main():
         sheet1.write(i, 0, value)
         sheet1.write(i, 1, count)
         sheet1.write(i, 2, projects)
-        i+=1
+        i += 1
     book.save("Components Statistics.xls")
+
+
+def main():
+    for BOMfilename in os.listdir("."):
+        if BOMfilename.split('.')[1] == 'csv':
+            i_value, data = get_safe_data(BOMfilename)
+            if data:
+                for row in data:
+                    value = row[i_value]
+                    if value in components.keys():
+                        components[value].append(BOMfilename.split('BOM')[0])
+                    else:
+                        components[value] = [BOMfilename.split('BOM')[0]]
+    data = [item for item in components.items() if not item[0] in dummies]
+    result = [(value, len(list(set(projects))), list(set(projects))) for (value, projects) in data]
+    result.sort(key=lambda x:x[1], reverse=True)
+    write_data_to_xls(result)
+
 
 if __name__ == '__main__':
     main()
